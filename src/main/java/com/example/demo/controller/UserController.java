0326c3e26d4c2aa3ec433672137ece2496e94f40
bala-amypo <bala@amypo.in>
2025-12-26 10:12:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,8 +4,7 @@ import com.example.demo.dto.UserResponse;
 import com.example.demo.model.User;
 import com.example.demo.service.impl.UserServiceImpl;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +16,13 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "*")
-@Tag(name = "Users-Controller", description = "Operations related to Users")
+
 public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
 
-    @Operation(summary = "Register user")
+    
     @PostMapping("/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody User user) {
 
@@ -34,7 +33,7 @@ public class UserController {
         );
     }
 
-    @Operation(summary = "List all users")
+    
     @GetMapping("/all")
     public ResponseEntity<List<UserResponse>> getAllUsers() {
 
@@ -46,7 +45,7 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
-    @Operation(summary = "Get user by ID")
+   
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
 
